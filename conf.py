11288fb0e4785323c36db0fa041ef65ed2584392
Lasyard <lasyard@yeah.net>
2026-09-11 11:38:07 +0800

    A badge class

--- conf.py
+++ conf.py
@@ -20,9 +20,7 @@ language = 'en'
 html_logo = '_images/lasydoc.png'
 html_favicon = '_images/favicon-96x96.png'
 
-myst_substitutions = {
-    "ai_main": '🤖以下内容为人工智能生成 | AI Generated Contents',
-    "ai_help": '✨以下内容借助人工智能编写 | AI Assisted Contents',
+myst_substitutions |= {
 }
 
 exclude_patterns += ['AGENTS.md']
